0420516c370c2a9741ec8cb7848ad2a3baa2eec1
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-23 19:56:52 +0200

    added getProfile and loadToken  func in auth service

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -23,6 +23,20 @@ export class AuthService {
       .map(res => res.json())
   }
 
+  getProfile(){
+    this.loadToken();
+    let headers = new Headers();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/auth', {headers: headers})
+      .map(res => res.json())
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   storeUserData(token, user){
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
