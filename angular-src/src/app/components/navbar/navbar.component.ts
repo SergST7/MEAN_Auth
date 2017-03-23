06959c04d78c2b352523d858cc197ab5f09cfebc
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-23 19:23:38 +0200

    added logout in navbar component

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FlashMessagesService} from "angular2-flash-messages";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private flashMessage: FlashMessagesService,
+              private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onLogout(){
+    this.authService.logout();
+    this.flashMessage.show('You are logged out ', { cssClass: 'alert-success', timeout: 3000 });
+  }
 }
